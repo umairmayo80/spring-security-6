f299f1bce43f8ff1fd568266036ce7e39538c3f1
umairmayo80 <aliumer1080@gmail.com>
2024-07-03 15:01:36 +0500

    spring security 6 project init

---
+++ spring-security/src/main/java/com/umair/spring_security/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.umair.spring_security;
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfiguration {
+}
